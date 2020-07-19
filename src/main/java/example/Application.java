636e4c0d2f3964cf227bd1c262801eddf11b2afa
gregwhitaker <gwhitake@gmail.com>
2020-07-19 13:58:57 -0700

    Doing work

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -4,8 +4,18 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Runs the example application.
@@ -25,13 +35,32 @@ public class Application {
                 .build();
 
         final DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
+
+        createTable(dynamoDB);
+        populateTable(dynamoDB);
     }
 
-    private void createTable() {
+    private void createTable(DynamoDB dynamoDB) {
+        LOG.info("Creating Table: {}", TABLE_NAME);
 
+        final List<KeySchemaElement> keySchema = new ArrayList<>();
+        keySchema.add(new KeySchemaElement("id", KeyType.HASH));
+
+        final List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
+        attributeDefinitions.add(new AttributeDefinition("id", ScalarAttributeType.S));
+
+        final CreateTableRequest createTableRequest = new CreateTableRequest();
+        createTableRequest.setTableName(TABLE_NAME);
+        createTableRequest.setKeySchema(keySchema);
+        createTableRequest.setAttributeDefinitions(attributeDefinitions);
+        createTableRequest.setProvisionedThroughput(new ProvisionedThroughput(1000L, 1000L));
+
+        Table table = dynamoDB.createTable(createTableRequest);
+
+        LOG.info("Created Table: {}", TABLE_NAME);
     }
 
-    private void populateTable() {
+    private void populateTable(DynamoDB dynamoDB) {
 
     }
 }
